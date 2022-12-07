43c30b10305b5cae6fc56b3c50fe98a6962e8370
JesterCharles <JesterCharles@gmail.com>
2022-12-07 10:03:48 -0500

    added cors for any orign as well as exposing authorization headers

--- src/main/java/com/revature/CustomerTracker/Application.java
+++ src/main/java/com/revature/CustomerTracker/Application.java
@@ -10,7 +10,16 @@ import io.javalin.Javalin;
 public class Application {
     public static void main(String[] args) {
 
-        Javalin app = Javalin.create().start(8080);
+        Javalin app = Javalin.create(config -> {
+                    config.plugins.enableCors(cors ->{
+                        cors.add(it -> {
+                            it.anyHost();
+                            it.exposeHeader("Authorization");
+                        });
+                    });
+                }
+
+        ).start(8080);
 
         JWTUtility jwtUtility = new JWTUtility();
         CustomerService customerService = new CustomerService(new CustomerDAO());
